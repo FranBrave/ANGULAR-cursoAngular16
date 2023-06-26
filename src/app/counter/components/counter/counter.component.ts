26704e3c6f679ed8127b002451a46e8bf26ac968
FranBrave <valiente.fco@gmail.com>
2023-06-26 21:06:56 +0200

    Fin de sección 4

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -17,7 +17,7 @@ import { Component } from '@angular/core';
 
 export class CounterComponent   {
 
-  public counter: number = 10;
+  public counter: number = 12;
 
   increaseBy( value: number ): void {
     this.counter += value
